4795ea821c2e5e4b711dca57ece69f3ac214bb75
selcuktemizsoy <selcuktemizsoy@192.168.1.36>
2020-06-14 18:41:57 +0300

    Narsist numbers calculation is done

--- Training Works/src/NarsistNumbers.java
+++ Training Works/src/NarsistNumbers.java
@@ -2,8 +2,24 @@
 public class NarsistNumbers {
 
 	public static void main(String[] args) {
+			
+		int number = 8208;
+		int result = number;
+		String value = number + "";
+		int length = String.valueOf(value).length();
+		int count = 0;
 		
-
+		for (int i = length; i > 0; i--) {
+			int pow = (int)Math.pow(10, i - 1);
+			int temp =  number / pow;
+			number = number % pow;
+			
+			count += Math.pow(temp, length);
+			
+		}
+		String isNarsist =(count == result) ? "Narsist" : "Not narsist";
+		System.out.println("Your number is " + result + " and your number is " + isNarsist);
+		
 	}
 
 }
